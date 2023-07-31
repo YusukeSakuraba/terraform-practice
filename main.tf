f738bc993839b405aa1ea3441b18b7ea559e4a00
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-01 01:25:34 +0900

    add s3 settings

--- main.tf
+++ main.tf
@@ -5,3 +5,72 @@ module "describe_regions_for_ec2" {
   identifier = "ec2.amazonaws.com"
   policy     = module.describe_regions_for_ec2.allow_describe_regions_policy
 }
+
+resource "aws_s3_bucket" "private" {
+  bucket = "private-tf-practice-bucket-yus"
+
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# ブロックパブリックアクセス
+# 予期しないオブジェクトの公開を抑止できる。特に理由がなければ全ての設定を有効にする
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# パブリックバケット
+resource "aws_s3_bucket" "public" {
+  bucket = "public-tf-practice-bucket-yus"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+# ログローテーションバケット（AWS各種サービスがログを保存するバケット）
+# 八章でALB使う時に使用予定
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-tf-practice-yus"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
