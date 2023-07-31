fd6d190146709b79a225f51e0666e0e762ba1a8d
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-01 01:01:27 +0900

    read iam module

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-module "web_server" {
-  source        = "./http_server"
-  instance_type = "t3.micro"
-}
 
-output "public_dns" {
-  value = module.web_server.public_dns
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = module.describe_regions_for_ec2.allow_describe_regions_policy
 }
