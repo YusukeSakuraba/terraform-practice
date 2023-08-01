ad9f9591ad57360a9f40d24251266292f5f527b3
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-01 23:35:47 +0900

    add nat gateway

--- main.tf
+++ main.tf
@@ -173,3 +173,14 @@ resource "aws_eip" "nat_gateway" {
     Name = "tf-eip"
   }
 }
+
+# NATゲートウェイ
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public.id
+  depends_on    = [aws_internet_gateway.example]
+
+  tags = {
+    Name = "tf-nat-gateway"
+  }
+}
