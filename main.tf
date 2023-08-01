7d842bc606a699a7c2304ab585e7cb8e6aad6031
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-01 23:23:11 +0900

    add private route

--- main.tf
+++ main.tf
@@ -152,3 +152,13 @@ resource "aws_subnet" "private" {
     Name = "tf-private-subnet"
   }
 }
+
+# ルートテーブルと関連付け
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
