db4036e2c16ee414e4d36cadf7323740378e81d6
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-01 23:10:12 +0900

    add route, association

--- main.tf
+++ main.tf
@@ -121,3 +121,16 @@ resource "aws_route_table" "public" {
     Name = "tf-route-table"
   }
 }
+
+# ルート
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# ルートテーブルの関連付け
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
