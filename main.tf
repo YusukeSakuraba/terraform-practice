587900948ba8626ce9d8ee498fc0bb66a62bc15f
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-02 22:24:34 +0900

    add sg rule

--- main.tf
+++ main.tf
@@ -259,3 +259,20 @@ resource "aws_security_group" "example" {
   vpc_id = aws_vpc.example.id
 }
 
+resource "aws_security_group_rule" "ingress_exxample" {
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+resource "aws_security_group_rule" "egress_exxample" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
