3959855c29a65177bd26d823c0221213abb64b93
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-02 09:16:52 +0900

    sff private route

--- main.tf
+++ main.tf
@@ -184,3 +184,11 @@ resource "aws_nat_gateway" "example" {
     Name = "tf-nat-gateway"
   }
 }
+
+# ルート
+# プライベートネットワークからネットへ通信するためにルートを定義
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
