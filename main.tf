1a8b97dc8a7dddc402c3b5af2918755d531b4e06
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-02 11:29:25 +0900

    update route table to multiAZ

--- main.tf
+++ main.tf
@@ -181,13 +181,36 @@ resource "aws_subnet" "private_1" {
 }
 
 # ルートテーブルと関連付け
-resource "aws_route_table" "private" {
+resource "aws_route_table" "private_0" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+}
+
+# ルート
+# プライベートネットワークからネットへ通信するためにルートを定義
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
 }
 
 # NATサーバーを導入するとプライベートネットワークからインターネットへアクセス可能になる
@@ -231,11 +254,4 @@ resource "aws_nat_gateway" "nat_gateway_1" {
   }
 }
 
-# ルート
-# プライベートネットワークからネットへ通信するためにルートを定義
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private.id
-  nat_gateway_id         = aws_nat_gateway.example.id
-  destination_cidr_block = "0.0.0.0/0"
-}
 
