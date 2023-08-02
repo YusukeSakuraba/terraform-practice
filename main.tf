1a8c1ffbe3149bdc2fd4b14def46c2553ed128ae
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-02 22:44:25 +0900

    read sg module

--- main.tf
+++ main.tf
@@ -254,25 +254,10 @@ resource "aws_nat_gateway" "nat_gateway_1" {
   }
 }
 
-resource "aws_security_group" "example" {
-  name   = "tf_sg"
-  vpc_id = aws_vpc.example.id
-}
-
-resource "aws_security_group_rule" "ingress_exxample" {
-  type              = "ingress"
-  from_port         = "80"
-  to_port           = "80"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
-}
-
-resource "aws_security_group_rule" "egress_exxample" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
 }
