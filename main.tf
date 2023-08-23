70eed6e05bd4ca206c83d9d2eb7367b123109326
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-23 20:19:11 +0900

    add ssl reocrd

--- main.tf
+++ main.tf
@@ -387,13 +387,21 @@ resource "aws_acm_certificate" "example" {
 
 # SSL証明書の検証
 # DNS検証用のDNSレコードを追加
-# resource "aws_route53_record" "example_certificate" {
-#   name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
-#   type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
-#   records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
-#   zone_id = data.aws_route53_zone.example.id
-#   ttl     = 60
-# }
+resource "aws_route53_record" "example_certificate" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  zone_id = data.aws_route53_zone.example.id
+  ttl     = 60
+}
 
 # resource "aws_acm_certificate_validation" "example" {
 #   certificate_arn         = aws_acm_certificate.example.arn
