f60ab595578f14306a894cb8c696881d5d470506
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-24 09:35:10 +0900

    add ecs_task_execution_role

--- main.tf
+++ main.tf
@@ -578,3 +578,11 @@ data "aws_iam_policy_document" "ecs_task_execution" {
     resources = ["*"]
   }
 }
+
+# ECSタスク実行IAMロールの定義
+module "ecs_task_execution_role" {
+  source     = "./iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
