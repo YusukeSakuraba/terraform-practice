da7b576424c4aa70da94e6d3d6c1da19f78c44a2
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-26 17:25:59 +0900

    add cw event target

--- main.tf
+++ main.tf
@@ -624,3 +624,23 @@ resource "aws_cloudwatch_event_rule" "example_batch" {
   description         = "実践TFで作成"
   schedule_expression = "cron(*/2 * * * ? *)"
 }
+
+# cloudwatchイベントターゲットで実行対象のジョブを定義
+resource "aws_cloudwatch_event_target" "example_batch" {
+  target_id = "example-batch"
+  rule      = aws_cloudwatch_event_rule.example_batch.name
+  role_arn  = module.ecs_events_role.iam_role_arn
+  arn       = aws_ecs_cluster.example.arn
+
+  ecs_target {
+    launch_type         = "FARGATE"
+    task_count          = 1
+    platform_version    = "1.3.0"
+    task_definition_arn = aws_ecs_task_definition.example_batch.arn
+
+    network_configuration {
+      assign_public_ip = "false"
+      subnets          = [aws_subnet.private_0.id]
+    }
+  }
+}
