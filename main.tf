997682e83d74cdda162a51484d88c9a7399419f1
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-27 15:09:28 +0900

    add elasticache replication group

--- main.tf
+++ main.tf
@@ -816,3 +816,34 @@ resource "aws_elasticache_subnet_group" "example" {
   name       = "example"
   subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
 }
+
+# ElastiCacheレプリケーショングループ
+# Redisサーバーを作成
+resource "aws_elasticache_replication_group" "example" {
+  replication_group_id          = "example"
+
+  replication_group_description = "Cluster Disabledです"
+  
+  engine                        = "redis"
+  engine_version                = "5.0.4"
+  
+  number_cache_clusters         = 3
+  node_type                     = "cache.m3.medium"
+  
+  snapshot_window               = "09:10--10:10"
+  snapshot_retention_limit      = 7
+  
+  maintenance_window            = "mon:10:40-mon:11:40"
+  
+  # フェイルオーバー；システムが停止した時に自動的に待機システムに切り替える仕組み
+  automatic_failover_enabled    = true
+  
+  port                          = 6379
+
+  apply_immediately             = false
+  
+  security_group_ids            = [module.redis_sg.security_group_id]
+  
+  parameter_group_name          = aws_elasticache_parameter_group.example.name
+  subnet_group_name             = aws_elasticache_subnet_group.example.name
+}
