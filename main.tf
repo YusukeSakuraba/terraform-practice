a0b327283f8349a2ce74e7765a5513ea81dce846
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-27 15:36:59 +0900

    add ecr

--- main.tf
+++ main.tf
@@ -856,3 +856,31 @@ module "redis_sg" {
   port        = 6379
   cidr_blocks = [aws_vpc.example.cidr_block]
 }
+
+# ECRリポジトリ
+resource "aws_ecr_repository" "example" {
+  name="example"
+}
+
+# ECRライフサイクルポリシー
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository=aws_ecr_repository.example.name
+
+  policy = <<EOF
+  {
+    "rules":[
+      "rulePriority":1,
+      "dexcription":"keep last 30 release tagged images",
+      "selection":{
+        "tagStatus":"tagged",
+        "tagPrefixList":["release"],
+        "countType":"imageCountMoreThan",
+        "countNumber":30
+      },
+      "action":{
+        "type":"expire"
+      }
+    ]
+  }
+  EOF
+}
